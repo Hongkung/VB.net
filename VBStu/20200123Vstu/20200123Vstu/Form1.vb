d90b18ca522fe6f0ec725db1c41ca43c8f11d556
Ryujaehong <woghd4244@gmail.com>
2020-01-24 00:25:12 +0900

    20200124
    Osaka

---
+++ VBStu/20200123Vstu/20200123Vstu/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Dim person As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        person = TextBox1.Text
+
+        BirthdayGreeing(person)
+
+    End Sub
+End Class
---
+++ VBStu/20200123Vstu/20200123Vstu/Test.vb
@@ -0,0 +1,11 @@
+﻿Module Test
+    Sub BirthdayGreeing(ByVal person As String)
+        Dim Msg As String
+        If person <> "" Then
+            Msg = "Happy birthday" & person & "!"
+        Else
+            Msg = "Name not specified"
+        End If
+        MsgBox(Msg, , "best wishes")
+    End Sub
+End Module

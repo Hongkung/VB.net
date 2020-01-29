1c8c72daa1c6aa4e88581b57daa2d6f4b75d97f5
Ryujaehong <woghd4244@gmail.com>
2020-01-30 01:06:12 +0900

    20200129
    DataGridView Study

---
+++ VBStu/20200129VBstu/20200129VBstu/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.add_btn = New System.Windows.Forms.Button()
+        Me.update_btn = New System.Windows.Forms.Button()
+        Me.delete_btn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.name_tb = New System.Windows.Forms.TextBox()
+        Me.address_tb = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'add_btn
+        '
+        Me.add_btn.Location = New System.Drawing.Point(638, 191)
+        Me.add_btn.Name = "add_btn"
+        Me.add_btn.Size = New System.Drawing.Size(75, 23)
+        Me.add_btn.TabIndex = 0
+        Me.add_btn.Text = "Button1"
+        Me.add_btn.UseVisualStyleBackColor = True
+        '
+        'update_btn
+        '
+        Me.update_btn.Location = New System.Drawing.Point(638, 220)
+        Me.update_btn.Name = "update_btn"
+        Me.update_btn.Size = New System.Drawing.Size(75, 23)
+        Me.update_btn.TabIndex = 0
+        Me.update_btn.Text = "Button1"
+        Me.update_btn.UseVisualStyleBackColor = True
+        '
+        'delete_btn
+        '
+        Me.delete_btn.Location = New System.Drawing.Point(638, 249)
+        Me.delete_btn.Name = "delete_btn"
+        Me.delete_btn.Size = New System.Drawing.Size(75, 23)
+        Me.delete_btn.TabIndex = 0
+        Me.delete_btn.Text = "Button1"
+        Me.delete_btn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(545, 103)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(545, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label1"
+        '
+        'name_tb
+        '
+        Me.name_tb.Location = New System.Drawing.Point(625, 98)
+        Me.name_tb.Name = "name_tb"
+        Me.name_tb.Size = New System.Drawing.Size(100, 25)
+        Me.name_tb.TabIndex = 2
+        '
+        'address_tb
+        '
+        Me.address_tb.Location = New System.Drawing.Point(625, 139)
+        Me.address_tb.Name = "address_tb"
+        Me.address_tb.Size = New System.Drawing.Size(100, 25)
+        Me.address_tb.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(24, 23)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(505, 401)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.address_tb)
+        Me.Controls.Add(Me.name_tb)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.delete_btn)
+        Me.Controls.Add(Me.update_btn)
+        Me.Controls.Add(Me.add_btn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents add_btn As Button
+    Friend WithEvents update_btn As Button
+    Friend WithEvents delete_btn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents name_tb As TextBox
+    Friend WithEvents address_tb As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ VBStu/20200129VBstu/20200129VBstu/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+
+    'dataGridView Test
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        setName()
+
+        'cell 선택시 해당셀이 속한 row전체가 선택된다. 
+        'When selecting a cell, the entire row to which the cell belongs is selected.
+        DataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+
+        DataGridView1.ColumnCount = 2
+        DataGridView1.Columns(0).Name = "name"
+        DataGridView1.Columns(1).Name = "Address"
+
+
+
+    End Sub
+
+    'add btn
+    Private Sub add_btn_Click(sender As Object, e As EventArgs) Handles add_btn.Click
+
+
+
+        If name_tb.Text = "" AndAlso address_tb.Text = "" Then
+            MsgBox("date input !", MsgBoxStyle.Exclamation)
+            Return
+        End If
+
+        Dim row As String() = New String() {name_tb.Text, address_tb.Text}
+
+        DataGridView1.Rows.Add(row)
+
+
+        name_tb.Text = ""
+        address_tb.Text = ""
+
+    End Sub
+
+
+
+    'update btn
+    Private Sub update_btn_Click(sender As Object, e As EventArgs) Handles update_btn.Click
+        DataGridView1.SelectedRows(0).Cells(0).Value = name_tb.Text
+        DataGridView1.SelectedRows(0).Cells(1).Value = address_tb.Text
+
+    End Sub
+
+    'delete btn
+    Private Sub delete_btn_Click(sender As Object, e As EventArgs) Handles delete_btn.Click
+
+        If DataGridView1.SelectedRows(0).Cells(0).Value = Nothing Then
+            Return
+        End If
+
+        Dim msgResult As MsgBoxResult = MsgBox("삭제하시겠습니까?", MsgBoxStyle.YesNo)
+
+        If msgResult = MsgBoxResult.Yes Then
+            DataGridView1.Rows.RemoveAt(DataGridView1.SelectedRows(0).Index)
+
+            name_tb.Text = ""
+            address_tb.Text = ""
+
+        End If
+    End Sub
+
+    'name set
+    Private Sub setName()
+        Label1.Text = "Name"
+        Label2.Text = "Address"
+
+        add_btn.Text = "add"
+        update_btn.Text = "update"
+        delete_btn.Text = "delete"
+
+    End Sub
+
+    'cell 클릭시 발생 이벤트 
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+
+        'DataGridViewCellEventArgs < 셀,행조작과 관련된 이벤트에 대한 데이터 제공
+        If (e.RowIndex > -1) And Not (DataGridView1.SelectedRows(0).Cells(0).Value = Nothing) Then
+
+            name_tb.Text = DataGridView1.SelectedRows(0).Cells(0).Value.ToString
+
+            address_tb.Text = DataGridView1.SelectedRows(0).Cells(1).Value.ToString
+        Else
+            name_tb.Text = ""
+            address_tb.Text = ""
+
+        End If
+
+
+
+    End Sub
+
+
+End Class

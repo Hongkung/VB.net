88144f4eef167a6f2547d72da30306e57c5ed4a3
Ryujaehong <woghd4244@gmail.com>
2020-05-06 21:23:59 +0900

    Sort
    Osaka 20200506

---
+++ VBStu/Sort/Sort/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(42, 37)
+        Me.TextBox1.MaxLength = 8
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(42, 110)
+        Me.TextBox2.MaxLength = 8
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 0
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(42, 183)
+        Me.TextBox3.MaxLength = 8
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox3.TabIndex = 0
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(379, 37)
+        Me.TextBox4.MaxLength = 8
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox4.TabIndex = 0
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(379, 110)
+        Me.TextBox5.MaxLength = 8
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox5.TabIndex = 0
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(379, 183)
+        Me.TextBox6.MaxLength = 8
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox6.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(224, 53)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 25)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(213, 110)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(115, 16)
+        Me.RadioButton1.TabIndex = 2
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "ascending order"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(213, 161)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(122, 16)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "descending order"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(545, 239)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Sort"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ VBStu/Sort/Sort/Form1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.Globalization
+
+Public Class Form1
+
+    Private value As ArrayList
+    Private ErrorMsg() As String
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        IndexCount()
+
+
+
+    End Sub
+
+    Private Sub IndexCount()
+
+        TextBox1.TabIndex = 0
+        TextBox2.TabIndex = 1
+        TextBox3.TabIndex = 2
+        TextBox4.TabIndex = 3
+        TextBox5.TabIndex = 4
+
+        RadioButton1.TabIndex = 6
+        RadioButton2.TabIndex = 7
+        Button1.TabIndex = 8
+
+    End Sub
+
+    Private Sub BasicsSet()
+
+        RadioButton1.Checked = True
+
+
+    End Sub
+
+    'Right Area
+    Private Sub SetNum(ByVal values As ArrayList, ByVal text As String)
+        Dim Ctext As Integer = Nothing
+
+        Try
+            Ctext = CInt(text)
+
+            If Ctext <> Nothing Then
+                values.Add(Ctext)
+            End If
+
+        Catch ex As Exception
+            MsgBox("SetNum Error", "ErrorMSG", MessageBoxButtons.OK)
+            Exit Sub
+        End Try
+
+    End Sub
+
+    'Left Area
+
+    Private Sub SetLeft(ByVal value As ArrayList)
+
+        If RadioButton1.Checked Then
+            Rdi1(value)
+            Output(value)
+
+        ElseIf RadioButton2.Checked Then
+            Rdi2(value)
+            Output(value)
+
+        End If
+
+    End Sub
+
+    Private Sub Rdi1(ByRef value As ArrayList)
+        Dim temp As Integer
+
+        For index = 0 To value.Count
+            For index2 = 0 To 1
+                If value(index2) > value(index2 + 1) Then
+                    temp = value(index2)
+                    value(index2) = value(index2 + 1)
+                    value(index2 + 1) = temp
+                End If
+            Next index2
+
+        Next index
+
+
+    End Sub
+
+    Private Sub Rdi2(ByRef value As ArrayList)
+        Dim temp As Integer
+
+        For index = 0 To value.Count
+            For index2 = 0 To 1
+                If value(index2) < value(index2 + 1) Then
+                    temp = value(index2)
+                    value(index2) = value(index2 + 1)
+                    value(index2 + 1) = temp
+                End If
+            Next index2
+
+        Next index
+
+
+    End Sub
+
+    Private Sub Output(ByVal value As ArrayList)
+        Dim count As Integer = value.Count
+
+        If 0 <= count - 1 AndAlso value(0) <> Nothing Then
+            TextBox4.Text = value(0)
+        End If
+        If 1 <= count - 1 AndAlso value(1) <> Nothing Then
+            TextBox5.Text = value(1)
+        End If
+        If 2 <= count - 1 AndAlso value(2) <> Nothing Then
+            TextBox6.Text = value(0)
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        value = New ArrayList
+        SetNum(value, TextBox1.Text)
+        SetNum(value, TextBox2.Text)
+        SetNum(value, TextBox3.Text)
+
+        SetLeft(value)
+
+
+    End Sub
+End Class

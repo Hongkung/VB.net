f67369e19e4d13e406ff9c0bede55a39510759b7
Ryujaehong <woghd4244@gmail.com>
2020-05-11 00:34:50 +0900

    Osaka
    2020/05/11

---
+++ VBStu/TimeCalulator/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(105, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(105, 139)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(248, 64)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(439, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(439, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(439, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 12)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Label1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(439, 44)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "TodayTime"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(439, 81)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 12)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "TomorrowTime"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(439, 118)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 12)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Time"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(62, 44)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(98, 12)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "입력형식) hhmm"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(543, 215)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ VBStu/TimeCalulator/WindowsApp2/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+
+    Private TodayTime As Integer '22 ~ 24시 사이의 시간
+    Private TomorrowTime As Integer '0~ 5시 사이의 시간
+    Private Time As Integer '플레이 타임
+    Private StartTime As Integer '시작시간
+    Private EndTime As Integer '끝나는 시간
+
+    Private Const Time_24 As Integer = 1440
+    Private Const Time_StartLine As Integer = 1320
+    Private Const Time_EndLine As Integer = 300
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        StartTime = CInt(TextBox1.Text.Trim)
+        EndTime = CInt(TextBox2.Text.Trim)
+
+        Calculator(StartTime, EndTime)
+
+        Label1.Text = TodayTime
+        Label2.Text = TomorrowTime
+        Label3.Text = Time
+    End Sub
+
+
+
+    Private Sub Calculator(ByVal st As Integer, ByVal et As Integer)
+
+        Dim stmin As Integer = st
+        Dim enmin As Integer = et
+
+        ConvertMin(stmin)
+        ConvertMin(enmin)
+
+        SetTime(stmin, enmin)
+        SetTodayTime(stmin, enmin)
+        SetTomorrowTime(stmin, enmin)
+
+    End Sub
+
+    Private Sub SetTime(ByVal smin As Integer, ByVal emin As Integer)
+        '총시간 계산
+        Try
+            If emin < smin Then
+                'endtime이 24 시간을 넘어 다음날이 될 경우
+                Time = emin + Time_24
+                Time = Time - smin
+            Else
+                Time = emin - smin
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error : SetTime")
+            Time = 0
+            Exit Sub
+        End Try
+
+    End Sub
+
+    Private Sub SetTodayTime(ByVal smin As Integer, ByVal emin As Integer)
+        Try
+            'endtime이 0 or smin 보다 작으면 24시로 고정
+            If emin = 0 Or emin < smin Then
+                emin = Time_24
+            End If
+
+            If smin < Time_StartLine Then
+                'starttime이 22시보다 작으면 22시로 고정
+                smin = Time_StartLine
+            End If
+            '위의 조건에 들어가지 않으면 22시~24시 안쪽의 시간대이므로 그냥 빼기
+
+            TodayTime = emin - smin
+
+            If TodayTime < 0 Then
+                TodayTime = 0
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error : SetTodayTime")
+            TodayTime = 0
+            Exit Sub
+        End Try
+
+    End Sub
+
+    Private Sub SetTomorrowTime(ByVal smin As Integer, ByVal emin As Integer)
+        Try
+            If emin > Time_EndLine Then
+                'emin이 5시 보다 크면 5시로 고정
+                emin = Time_EndLine
+            End If
+            If emin < smin Then
+                ' smin이 emin보다 크면 24시로 고정
+                smin = 0
+            End If
+
+            TomorrowTime = emin - smin
+
+            If TomorrowTime < 0 Then
+                TomorrowTime = 0
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error : SetTomorrowTime")
+            TomorrowTime = 0
+            Exit Sub
+        End Try
+
+    End Sub
+
+    Private Sub ConvertMin(ByRef time As Integer)
+
+        If time <> 0 Then
+
+            If time < 100 Then
+                time = time Mod 100
+            Else
+                time = ((time \ 100) * 60) + time Mod 100
+            End If
+
+        End If
+
+    End Sub
+End Class
